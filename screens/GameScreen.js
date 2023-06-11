dbb2e6c1b9be1ffea925a8782e780e0a1b76a41d
Krippykesh <gayyyy>
2023-06-11 16:40:42 +0530

    orientations optimised

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, SafeAreaView, Alert } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, Alert, FlatList,useWindowDimensions } from "react-native";
 import { useEffect, useState } from "react";
 import {Ionicons} from '@expo/vector-icons'
 import Title from "../components/ui/Title";
@@ -6,6 +6,7 @@ import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/card";
 import Colors from "../constants/colors";
+import GuessLogItem from "../components/game/GuessLogItem";
 
 
 function generateRandomBetween(min, max, exclude) {
@@ -24,12 +25,20 @@ let maxBoundary = 100;
 function GameScreen({ userNumber, onGameOver }) {
   const initialGuess = generateRandomBetween(1, 100, userNumber);
   const [currentGuess, setcureentGuess] = useState(initialGuess);
+  const [guessRounds,setGuessRounds]=useState([initialGuess]);
+
+  const {width,height}=useWindowDimensions();
 
   useEffect(() => {
     if (currentGuess === userNumber) {
-      onGameOver();
+      onGameOver(guessRounds.length);
     }
   }, [currentGuess, userNumber, onGameOver]);
+
+  useEffect(()=>{
+    minBoundary=1;
+    maxBoundary=100;
+  },[]);
 
   function nextGuessHandler(direction) {
     if (
@@ -52,14 +61,16 @@ function GameScreen({ userNumber, onGameOver }) {
       currentGuess
     );
     setcureentGuess(newRndNummber);
+    setGuessRounds(prevGuessRounds =>[newRndNummber,...prevGuessRounds]);
   }
 
-  return (
-    <View style={styles.screen}>
-      <Title>Opponent's Guess</Title>
-      <NumberContainer>{currentGuess}</NumberContainer>
+  const guessRoundListLength=guessRounds.length;
+
+  let content =(
+    <>
+     <NumberContainer>{currentGuess}</NumberContainer>
       <Card>
-        <Text style={styles.instructionText}>Higher or Lower</Text>
+      <Text style={styles.instructionText}>Higher or Lower</Text>
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
           <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
@@ -73,6 +84,36 @@ function GameScreen({ userNumber, onGameOver }) {
           </View>
         </View>
       </Card>
+      </>
+  );
+   if(width>500){
+    content=(
+      <>
+         <View style={styles.buttonContainerwide}>
+         <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+            <Ionicons name="md-remove" size={24} color='white' />
+          </PrimaryButton>
+          </View>
+          <NumberContainer>{currentGuess}</NumberContainer>
+          <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+          <Ionicons name="md-add" size={24} color='white' />
+          </PrimaryButton>
+          </View>
+         </View>
+      </>
+    )
+   }
+
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      {content}
+      <View style={styles.listContainer}>
+        <FlatList data={guessRounds} renderItem={(itemData)=><GuessLogItem roundNumber={guessRoundListLength- itemData.index} guess={itemData.item}/>}
+        keyExtractor={(item)=>item}/>
+      </View>
     </View>
   );
 }
@@ -83,6 +124,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 35,
+    alignItems:'center',
   },
     instructionText: {
       color: Colors.accent500,
@@ -96,5 +138,13 @@ const styles = StyleSheet.create({
     buttonContainer: {
       flex: 1,
     },
+    listContainer:{
+      flex:1,
+      padding:10 ,
+    },
+    buttonContainerwide:{
+      flexDirection:'row',
+      alignItems:'center',
+    }
     
 });
